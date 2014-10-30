f339c5a3e1d4da9865e3f958e468c7fbbca955e0
FXHibon <hibonfx@gmail.com>
2014-10-30 19:09:01 +0100

    Initial commit

--- main.php
+++ main.php
@@ -8,8 +8,30 @@
 
 session_start();
 
+if (!isset($_SESSION["plateau"])) {
+    $_SESSION["plateau"] = array(array());
+    for ($i = 0; $i < 7; $i++) {
+        for ($j = 0; $j < 7; $j++) {
+            $_SESSION["plateau"][$i][$j] = true;
+        }
+    }
+}
+
 include("head.html");
 
+echo "<table>\n";
+
+for ($i = 0; $i < 7; $i++) {
+    echo "\t<tr>";
+    for ($j = 0; $j < 7; $j++) {
+        echo "<td>";
+        echo $_SESSION["plateau"][$i][$j];
+        echo "</td>";
+    }
+    echo "</tr>\n";
+}
+
+echo "</table>\n";
 
 
 include("bottom.html");
